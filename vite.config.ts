8b47582ed17f0e6bd7fcb94a059b677c7df6b54f
justinenjama <justinenjama@gmail.com>
2025-06-15 22:38:27 +0300

    Update 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  define: {
-    global: 'window',
-  },
+    base: '/Bank-System-Frontend/',
+    plugins: [react(), tailwindcss()],
+    define: {
+        global: 'window',
+    },
 })
